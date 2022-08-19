b53e9fe86abc74d9d3569433fac4fc94bde8215d
github username <um_2003_8@hotmail.com>
2022-08-19 15:49:05 -0400

    create a Pig class that implements Animal interface and add a implement methods to the class

--- java-interface/src/App.java
+++ java-interface/src/App.java
@@ -6,6 +6,22 @@ interface Animal {
 	public void sleep(); // interface method (does not have a body)
 }
 
+//Pig "implements" the Animal interface
+class Pig implements Animal {
+	@Override
+	public void animalSound() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sleep() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
 public class App {
 	public static void main(String[] args) {
 
