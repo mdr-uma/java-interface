ecd6823de5b9c377198f996300530ad17129ba88
github username <um_2003_8@hotmail.com>
2022-08-19 15:43:58 -0400

    add a example methods in interface Animal

--- java-interface/src/App.java
+++ java-interface/src/App.java
@@ -2,7 +2,8 @@
 //An interface is a completely "abstract class" that is used to group related methods with empty bodies.
 
 interface Animal {
-	
+	public void animalSound(); // interface method (does not have a body)
+	public void sleep(); // interface method (does not have a body)
 }
 
 public class App {
