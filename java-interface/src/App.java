facde4039ae60dc4c105ca204e84cdda1add78bc
github username <um_2003_8@hotmail.com>
2022-08-19 15:51:09 -0400

    add the body in the methods

--- java-interface/src/App.java
+++ java-interface/src/App.java
@@ -10,14 +10,14 @@ interface Animal {
 class Pig implements Animal {
 	@Override
 	public void animalSound() {
-		// TODO Auto-generated method stub
-		
+		// The body of animalSound() is provided here
+	    System.out.println("The pig says: wee wee");	
 	}
 
 	@Override
 	public void sleep() {
-		// TODO Auto-generated method stub
-		
+		// The body of sleep() is provided here
+	    System.out.println("Zzz");
 	}
 }
 
