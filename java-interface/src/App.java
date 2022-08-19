718681f97d281638273bb4d2e3d84d4e169f047e
github username <um_2003_8@hotmail.com>
2022-08-19 15:52:08 -0400

    create an object of a Pig class and run the interface methods

--- java-interface/src/App.java
+++ java-interface/src/App.java
@@ -24,6 +24,9 @@ class Pig implements Animal {
 
 public class App {
 	public static void main(String[] args) {
+		Pig myPig = new Pig();  // Create a Pig object
+	    myPig.animalSound();
+	    myPig.sleep();
 
 	}
 }
